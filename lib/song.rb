da3bc7db69b9844633d4dbb8af460ec2e5e751d8
Victor Williams <victor.williams1030@gmail.com>
2020-05-27 16:49:56 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name,artist,genre)
+    @name = name 
+    @artist = artist 
+    @@artists.push artist
+    @genre = genre
+    @@genres.push genre
+    @@count += 1
+  end
+  
+  def self.count
+    @@count 
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres 
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      genre_hash[genre]?
+    end
+  end
+  
+  
+end
