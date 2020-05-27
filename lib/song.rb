5f7a577569d83f29a3a0de804d197776cd31464b
Victor Williams <victor.williams1030@gmail.com>
2020-05-27 16:54:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,13 +28,12 @@ class Song
   def self.genre_count
     genre_hash = {}
     @@genres.each do |genre|
-      genre_hash[genre]?
+      if genre_hash[genre]?
         genre_hash[genre] += 1
       else 
         genre_hash[genre] = 1
       end
       genre_hash
-    end
   end
   
 end
